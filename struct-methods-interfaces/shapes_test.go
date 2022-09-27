1871e8163b1ebc486dec789ee59deba732d61832
radekska <r.skalbania@piwik.pro>
2022-09-27 19:13:39 +0200

    add triangle shape and make test cases named - so cool!

--- struct-methods-interfaces/shapes_test.go
+++ struct-methods-interfaces/shapes_test.go
@@ -12,22 +12,32 @@ func TestPerimeter(t *testing.T) {
 
 func TestArea(t *testing.T) {
 	areaTests := []struct {
+		name  string
 		shape Shape
 		want  float64
 	}{
 		{
+			name:  "Rectangle",
 			shape: Rectangle{Height: 12, Width: 6},
 			want:  72.0,
 		},
 		{
+			name:  "Circle",
 			shape: Circle{Radius: 10},
 			want:  314.1592653589793,
+		},
+		{
+			name:  "Triangle",
+			shape: Triangle{Base: 10, Height: 8},
+			want:  40.0,
 		},
 	}
 
 	for _, testCase := range areaTests {
-		got := testCase.shape.Area()
-		assert.Equal(t, testCase.want, got)
+		t.Run(testCase.name, func(t *testing.T) {
+			got := testCase.shape.Area()
+			assert.Equal(t, testCase.want, got)
+		})
 	}
 
 }
