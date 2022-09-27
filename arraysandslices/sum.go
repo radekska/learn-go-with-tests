a6d25d290e6d8d5f67b8cc292a8c0298a44bf4e5
Radosław Skałbania <48757764+radekska@users.noreply.github.com>
2022-09-27 18:35:34 +0200

    4 arrays and slices (#4)

---
+++ arraysandslices/sum.go
@@ -0,0 +1,30 @@
+package arraysandslices
+
+func Sum(numbers []int) int {
+	var sum int
+	for _, number := range numbers {
+		sum += number
+	}
+	return sum
+}
+
+func SumAll(numbersCollection ...[]int) []int {
+	var sums []int
+	for _, numbers := range numbersCollection {
+		sums = append(sums, Sum(numbers))
+	}
+	return sums
+}
+
+func SumAllTails(numbersCollection ...[]int) []int {
+	var sums []int
+	for _, numbers := range numbersCollection {
+		if len(numbers) == 0 {
+			sums = append(sums, 0)
+		} else {
+			sums = append(sums, Sum(numbers[1:]))
+		}
+
+	}
+	return sums
+}
---
+++ arraysandslices/sum_test.go
@@ -0,0 +1,46 @@
+package arraysandslices
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestSum(t *testing.T) {
+	t.Run("collection of 5 numbers", func(t *testing.T) {
+		numbers := []int{1, 2, 3, 4, 5}
+		assert.Equal(t, 15, Sum(numbers))
+	})
+
+}
+
+func TestSumAll(t *testing.T) {
+	t.Run("sums the single slice", func(t *testing.T) {
+		assert.Equal(t, []int{50}, SumAll([]int{10, 20, 20}))
+	})
+
+	t.Run("sums the multiple slice", func(t *testing.T) {
+		sliceOne := []int{10, 20, 20}
+		sliceTwo := []int{5, 10, 15, 20}
+		sliceThree := []int{8, 12}
+		assert.Equal(t, []int{50, 50, 20}, SumAll(sliceOne, sliceTwo, sliceThree))
+	})
+}
+
+func TestSumAllTails(t *testing.T) {
+	t.Run("sums the single slice", func(t *testing.T) {
+		assert.Equal(t, []int{40}, SumAllTails([]int{10, 20, 20}))
+	})
+
+	t.Run("sums the multiple slice", func(t *testing.T) {
+		sliceOne := []int{10, 20, 20}
+		sliceTwo := []int{5, 10, 15, 20}
+		sliceThree := []int{8, 12}
+		assert.Equal(t, []int{40, 45, 12}, SumAllTails(sliceOne, sliceTwo, sliceThree))
+	})
+
+	t.Run("safely sum empty slices", func(t *testing.T) {
+		got := SumAllTails([]int{}, []int{3, 4, 5})
+		want := []int{0, 9}
+		assert.Equal(t, want, got)
+	})
+}
