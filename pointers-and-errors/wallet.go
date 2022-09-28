3d672b5b1f0d6032038ec3c6ce51451d880edc36
Radosław Skałbania <48757764+radekska@users.noreply.github.com>
2022-09-28 21:28:35 +0200

    add wallet & tests (#6)

---
+++ pointers-and-errors/wallet.go
@@ -0,0 +1,27 @@
+package pointers_and_errors
+
+import "errors"
+
+type Bitcoin int
+
+type Wallet struct {
+	balance Bitcoin
+}
+
+func (w *Wallet) Deposit(amount Bitcoin) {
+	w.balance += amount
+}
+
+func (w *Wallet) Balance() Bitcoin {
+	return w.balance
+}
+
+var ErrInsufficientFunds = errors.New("insufficient funds")
+
+func (w *Wallet) Withdraw(amount Bitcoin) error {
+	if w.balance < amount {
+		return ErrInsufficientFunds
+	}
+	w.balance -= amount
+	return nil
+}
