d4e89bec366ea34fc57d39b3b5ee21c1767ba4b5
radekska <r.skalbania@piwik.pro>
2022-10-02 19:17:06 +0200

    add dependency injection examples

---
+++ dependecy-injection/dj_test.go
@@ -0,0 +1,18 @@
+package dependecy_injection
+
+import (
+	"bytes"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestGreet(t *testing.T) {
+	buffer := bytes.Buffer{}
+
+	Greet(&buffer, "Chris")
+
+	got := buffer.String()
+	want := "Hello, Chris"
+
+	assert.Equal(t, want, got)
+}
