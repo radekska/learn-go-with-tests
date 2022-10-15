4d3965c306dec53792046499108e3c17b7c82c72
radekska <r.skalbania@piwik.pro>
2022-10-15 15:36:16 +0200

    finish reflection example

--- reflection/reflection.go
+++ reflection/reflection.go
@@ -5,6 +5,8 @@ import "reflect"
 // Reflection in computing is the ability of a program to examine its own structure,
 // particularly through types; it's a form of metaprogramming.
 // It's also a great source of confusion.
+
+// Now that you know about reflection, do your best to avoid using it.
 func walk(x interface{}, fn func(input string)) {
 	val := getValue(x)
 
@@ -26,6 +28,15 @@ func walk(x interface{}, fn func(input string)) {
 	case reflect.Map:
 		for _, key := range val.MapKeys() {
 			walkValue(val.MapIndex(key))
+		}
+	case reflect.Chan:
+		for v, ok := val.Recv(); ok; v, ok = val.Recv() {
+			walkValue(v)
+		}
+	case reflect.Func:
+		valFnResult := val.Call(nil)
+		for _, res := range valFnResult {
+			walkValue(res)
 		}
 	}
 }
