f3202165752c0578e9706ac085a3ba1ae8976091
Radosław Skałbania <48757764+radekska@users.noreply.github.com>
2022-10-22 15:08:27 +0200

    work on context example (#14)

---
+++ context/context.go
@@ -0,0 +1,25 @@
+package context
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+type Store interface {
+	Fetch(ctx context.Context) (string, error)
+}
+
+func Server(store Store) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		data, err := store.Fetch(r.Context())
+
+		if err != nil {
+			log.Println(err)
+			return
+		}
+
+		fmt.Fprint(w, data)
+	}
+}
